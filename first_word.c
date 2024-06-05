3ae4e7376b454c0436ba7dc8e01c252d70c9edfc
Artur Carvalho <160274976+Artur-2k@users.noreply.github.com>
2024-06-05 14:59:57 +0100

    Andes first_word.c
    Didn’t like that the subject only asked for tabs spaces and newlines instead of whitespaces.

---
+++ first_word.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+int     is_whitespace(char c)
+{
+    if (c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '\f' || c == '\v')
+        return (1);
+    return (0);
+}
+
+void    print_first_word(char *str)
+{
+    while (*str && is_whitespace(*str))
+        str++;
+    if (!*str)
+        return ;
+    while (*str && !is_whitespace(*str))
+    {
+        write(1, str, 1);
+        str++;
+    }
+    write(1, "\n", 1);
+    return ;
+}
+
+int     main(int ac, char **av)
+{
+    av[1] = "   ";
+    if (ac != 2 && (!av[1] || !av[1][0]))
+    {
+        write(1, "\n", 1);
+        return (0);
+    }
+    print_first_word(av[1]);
+    return 0;
+}
