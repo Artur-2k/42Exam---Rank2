d4721f4b4fc207c64fed005722a93f2357f24e82
Artur Carvalho <arturcarvalho0507@gmail.com>
2024-06-29 18:07:51 +0100

    added expand_str

---
+++ Level-3/expand_str/expand_str.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+int is_white_space(char c)
+{
+	if (c == '\n' || c == '\t' || c == '\v' || c == ' ')
+		return 1;
+	return 0;
+}
+int main(int ac, char** av)
+{
+	if (ac != 2 || !av[1] || !av[1][0])
+		return (write(1, "\n", 1), 0);
+
+	int flag = 0;
+
+	while (*av[1] && is_white_space(*av[1]))
+		av[1]++;
+
+	while(*av[1])
+	{
+		while (*av[1] && is_white_space(*av[1]))
+		{
+			flag = 1;
+			av[1]++;
+		}
+		if (*av[1] && flag)
+			write(1, "   ", 3);
+		while(*av[1] && !is_white_space(*av[1]))
+		{
+			flag = 0;
+			write(1, &(*av[1]), 1);
+			av[1]++;
+		}
+	}
+	write(1 ,"\n", 1);
+}
