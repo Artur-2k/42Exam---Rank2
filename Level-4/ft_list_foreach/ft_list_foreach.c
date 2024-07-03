d786ead268048c6537fd0b9579f55ca9bd65074b
Artur Carvalho <arturcarvalho0507@gmail.com>
2024-07-03 17:31:30 +0100

    added ft_list_foreach

---
+++ Level-4/ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,13 @@
+#include "ft_list.h"
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	if (!begin_list || !f)
+		return ;
+
+	while (begin_list)
+	{
+		(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
